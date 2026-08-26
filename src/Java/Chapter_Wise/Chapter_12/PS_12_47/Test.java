c65fadfc35fe488a2d13ecd922f1cac8380eddd3
theVanshDhariwal <vanshdhariwal343@gmail.com>
2026-08-26 10:44:30 +0530

    Delete src/Java/Chapter_Wise directory.

--- src/Java/Chapter_Wise/Chapter_12/PS_12_47/Test.java
+++
@@ -1,9 +0,0 @@
-package Java.Chapter_Wise.Chapter_12.PS_12_47;
-
-import Java.Chapter_Wise.Chapter_12.PS_12_47.Problem_03.Folder1.Folder2.Folder3.PackageInCLass;
-
-public class Test {
-    public static void main(String[] args) {
-        PackageInCLass packageInCLass = new PackageInCLass();
-    }
-}
--- src/Java/Chapter_Wise/Test.java
+++
@@ -1,7 +0,0 @@
-package Java.Chapter_Wise;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Suryavanshi");
-    }
-}
